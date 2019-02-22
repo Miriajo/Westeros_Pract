fbf3a82e7760d4705d6cf652297efee5e3852091
Miriam Sánchez <miriam.sanchez.antona@gmail.com>
2019-02-22 04:24:36 +0100

    Implementado el SeasonList y SeasonDetail (punto 5)

--- Westeros/Controllers/SeasonDetailViewController.swift
+++ Westeros/Controllers/SeasonDetailViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class SeasonDetailViewController: UIViewController {
+class SeasonDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var seasonTitleLabel: UILabel!
     @IBOutlet weak var seasonPosterImg: UIImageView!
     @IBOutlet weak var seasonDateLabel: UILabel!
+    
+    @IBOutlet weak var seasonEpisodeList: UITableView!
     
     // MARK: Properties
     let model: Season
@@ -29,6 +31,11 @@ class SeasonDetailViewController: UIViewController {
     }
     
     // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.seasonEpisodeList.dataSource = self
+        self.seasonEpisodeList.delegate = self
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         syncModelWithView()
@@ -42,10 +49,51 @@ class SeasonDetailViewController: UIViewController {
        // Indicar el formato de visualización de la fecha y convertirla a String
         let displayFormatter = DateFormatter()
         displayFormatter.dateFormat = "dd-MMMM-y"
-        seasonDateLabel.text = displayFormatter.string(from: model.airedDate)
+        seasonDateLabel.text = "Live Date: \n" + displayFormatter.string(from: model.airedDate)
+        
+        // Mostrar la lista de episodios
+//        seasonEpisodesList.dataSource = model.sortedEpisodes as? UITableViewDataSource
+//        seasonEpisodesList.reloadData()
     }
     
     
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        return "Episodes"
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return model.sortedEpisodes.count
+    }
+    
+    
+   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Descubri la season a mostrar
+        let episode = model.sortedEpisodes[indexPath.row]
+        
+        // Crear la celda
+        let cellId = "EpisodeCell"
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellId)
+    
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellId)
+        }
+    
+        // Sincronizar celda-modelo
+        cell?.textLabel?.text = episode.description + " - " + episode.title
+        // Devolver la celda
+        return cell!
+    }
+    
+    // MARK: UITableView Delegate
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 20
+    }
     
     
 }
